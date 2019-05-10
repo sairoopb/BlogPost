30ae5388e7ff6f71a3d416c81d71428798ec477a
sairoop <sairoopb@gmail.com>
2019-05-10 19:32:04 +0530

    Changed the html to form type

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .forms import PostForm
 
 urlpatterns=[
-    path('',views.post_list,name='post_list')
+   # path('',views.post_list,name='post_list'),
+    path('post/new/', views.post_new, name='post_new')
 ]
