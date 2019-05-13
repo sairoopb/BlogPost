e4b246c5c989967f58be39aac93acf9276961af7
sairoop <sairoopb@gmail.com>
2019-05-13 12:14:34 +0530

    Made entering changes

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from .forms import PostForm
 
 urlpatterns=[
    # path('',views.post_list,name='post_list'),
-    path('post/new/', views.post_new, name='post_new')
+    path('', views.post_new, name='post_new')
 ]
